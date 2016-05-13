c81bfaccfea691139993cb8012941a8fdd8d4b45
Philip Newborough <corenominal@corenominal.org>
2016-05-13 07:57:19 +0100

    Today hour by hour report

--- endpoints/stats.php
+++ endpoints/stats.php
@@ -20,18 +20,29 @@ function iewp_crunchstats_endpoint_stats( $request_data )
 
 	// Test report type
 	if( !isset( $data['report'] ) || empty( $data['report'] ) )
-		return array( 'Error' => 'Please specify a report type. e.g. "report=summary-today"' );
+		return array( 'Error' => 'Please specify a report type. e.g. "report=today-hour-by-hour"' );
 
 	// Report switch
 	switch ( $data['report'] )
 	{
-		case 'summary-today':
-			return array('foo' => 'bar' );
+		// A hour by hour breakdown of today's hits
+		case 'today-hour-by-hour':
+			$sql = "SELECT FROM_UNIXTIME(`date`,'%k') AS `hour`, COUNT(*) AS `total`
+					  FROM `iewp_crunchstats_log`
+					  WHERE `is_bot` = 0 AND FROM_UNIXTIME(`date`,'%Y-%m-%d') = '" . date( 'Y-m-d' ) . "'
+					  GROUP BY `hour`
+					  ORDER BY date ASC";
+			$data['report'] = $wpdb->get_results( $sql, ARRAY_A );
+			$data['num_rows'] = $wpdb->num_rows;
 			break;
 		
 		default:
 			return array( 'Error' => 'Unknown report type. Please specify a valid report type' );
 			break;
 	}
+
+	// Remove API key and return report
+	unset( $data['apikey'] );
+	return $data;
 
 }
